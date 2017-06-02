220f7e6e195f8f0edcbdf6fbcf98c5905d2a0e08
M4dness <them4dman@gmail.com>
2017-06-02 02:49:13 -0500

    Addex Camera Feature

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -22,11 +22,42 @@ function ($scope, $stateParams) {
 
 }])
    
-.controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('menuCtrl', ['$scope', '$rootScope', '$state', '$stateParams', '$ionicHistory','$cordovaCamera', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-
+function ($scope, $rootScope, $state, $stateParams, $ionicHistory, $cordovaCamera) {
+    var options;
+    document.addEventListener("deviceready", function () {
+        options = {
+          quality: 90,
+          destinationType: Camera.DestinationType.DATA_URL,
+          sourceType: Camera.PictureSourceType.CAMERA,
+          allowEdit: true,
+          encodingType: Camera.EncodingType.JPEG,
+          targetWidth: 500,
+          targetHeight: 500,
+          popoverOptions: CameraPopoverOptions,
+          saveToPhotoAlbum: false,
+          correctOrientation:true
+        };
+    });
+    $scope.takePicture = function(){
+         $cordovaCamera.getPicture(options).then(function(imageData) {
+          //var image = document.getElementById('myImage');
+          //image.src = "data:image/jpeg;base64," + imageData;
+             if(imageData){
+                $rootScope.uploadedImage = "data:image/jpeg;base64," + imageData;
+                 $rootScope.uploadPost = true;
+             }
+            
+             $ionicHistory.nextViewOptions({
+                disableBack: true
+              });
+            $state.go('menu.feed');
+        }, function(err) {
+          // error
+        });   
+    }
 }])
    
 .controller('searchCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
